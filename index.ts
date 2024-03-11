6c23865f67e6db5791f574848f6b42b86d9e45ee
Jacob Reppuhn <jreppuhn@three-m.com>
2024-03-11 11:19:49 -0400

    dynamic url switch

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import puppeteer from 'puppeteer';
+import urls from './urls.json';
 
-const delay = (time: number) => new Promise((res) => setTimeout(res, time));
+const delay = (time: number): Promise<void> => new Promise((res) => setTimeout(res, time));
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -8,27 +9,22 @@ const delay = (time: number) => new Promise((res) => setTimeout(res, time));
     args: ['--start-fullscreen', '--kiosk'],
   });
 
-  const page1 = await browser.newPage();
-  await page1.setViewport({ width: 1920, height: 1080 });
-  await page1.goto(
-    'https://3mtool-my.sharepoint.com/:x:/g/personal/jreppuhn_three-m_com/ET9Lsbi09LJGv9pQcQJoq-ABltLtwsO0UFYarttJ6C_K2w'
+  const pages = await Promise.all(
+    urls.map(async (url: string) => {
+      const page = await browser.newPage();
+      await page.setViewport({ width: 1920, height: 1080 });
+      await page.goto(url);
+      await delay(10000);
+      return page;
+    })
   );
-  await delay(30000);
-
-  const page2 = await browser.newPage();
-  await page2.setViewport({ width: 1920, height: 1080 });
-  await page2.goto('https://dakboard.com/screen/uuid/611d7bad-117325-fd31-f280835381df');
-  await delay(10000);
 
   const waitTime = 15000;
 
   while (true) {
-    await page1.bringToFront();
-    console.log(`Switching to the first tab`);
-    await delay(waitTime);
-
-    await page2.bringToFront();
-    console.log(`Switching to the second tab`);
-    await delay(waitTime);
+    for (const page of pages) {
+      await page.bringToFront();
+      await delay(waitTime);
+    }
   }
 })();
