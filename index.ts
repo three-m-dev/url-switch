3b53fbef3551b91d078b6ab0ff23685145e60117
Jacob Reppuhn <jreppuhn@three-m.com>
2024-03-14 10:19:43 -0400

    code change

--- index.ts
+++ index.ts
@@ -7,26 +7,13 @@ const delay = (time: number): Promise<void> => new Promise((res) => setTimeout(r
   const browser = await puppeteer.launch({
     headless: false,
     executablePath: '/usr/bin/chromium-browser',
-    args: ['--no-sandbox', '--disable-setuid-sandbox', '--start-maximized', '--kiosk', '--disable-infobars'],
-  });
-
-  const page = await browser.newPage();
-
-  const context = browser.defaultBrowserContext();
-  context.overridePermissions('https://your-url-here.com', ['notifications']);
-
-  await page.goto('about:blank');
-  const dimensions = await page.evaluate(() => {
-    return {
-      width: window.screen.availWidth,
-      height: window.screen.availHeight,
-    };
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--start-fullscreen', '--kiosk', '--app'],
   });
 
   const pages = await Promise.all(
     urls.map(async (url: string) => {
       const page = await browser.newPage();
-      await page.setViewport({ width: dimensions.width, height: dimensions.height });
+      await page.setViewport({ width: 1920, height: 1080 });
       await page.goto(url);
       await delay(10000);
       return page;
